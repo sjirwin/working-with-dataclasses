6e765ba474463d39aea61607f932ad49e58b2a10
Scott Irwin <sirwin@rocketmail.com>
2019-03-19 08:56:46 -0400

    keep TODO comments in ex01.py

--- solutions/01/ex01.py
+++ solutions/01/ex01.py
@@ -6,6 +6,7 @@ class NoDefault:
 
 @dataclass
 class WithDefault:
+    # TODO: add the expected values as field defaults
     fld_1: int = 42
     fld_2: str = 'hello'
 
@@ -13,6 +14,7 @@ class WithDefault:
 class WithMethod:
     fld_1: float = 1.41421356
     fld_2: float = 0.70710678
-    
+
+    # TODO: implement fld_sum() function
     def fld_sum(self):
         return self.fld_1 + self.fld_2
